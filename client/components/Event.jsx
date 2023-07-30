16c090335e7c089201514f7861ba4a9b730cf4a2
Parwinder Singh <pinta.e86@gmail.com>
2023-07-29 23:28:20 -0400

    added cards

--- client/components/Event.jsx
+++ client/components/Event.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 
-import { Card, Box, Typography, Button } from "@mui/material";
+import { Card, Box, Typography, cardContent ,Button, sizing } from "@mui/material";
 
 
 
-function Event(){
+function Event( {eventName, User, day, start, end}){
 
     return (
-        <Card>
-            <Typography>
-                User:
-            </Typography>
-            <Typography>
-                User:
-            </Typography>
-            <Typography>
-                User:
-            </Typography>
+        <Card sx={{ borderBottom: 1, my: '1rem', boxShadow: 3,  width: "25%", p: '.5rem'}}>
+            <cardContent>
 
+                <Typography sx={{ mt: '1rem'}}>
+                    {/* User : {} */}
+                   <strong> User </strong>: Pinta
+                </Typography>
+                <Typography>
+                    <strong>Event </strong>: Dinner
+                    {/* Event : {eventName} */}
+                </Typography>
+                <Typography>
+                <strong>Avalibality</strong>: Monday : 2:00 - 3:00
+                </Typography>
+
+            </cardContent>
         </Card>
 
     )
--- client/components/EventList.jsx
+++ client/components/EventList.jsx
@@ -1,12 +1,56 @@
 import React from 'react';
 import Event from './Event';
+import { useState, useEffect } from 'react';
+
+//Usestate 
 
 function EventList() {
-  // const dummyEventList = [];
-  // for (let i = 0; i < 5; i++) dummyEventList.push(<Event key={crypto.randomUUID()}/>);
+  const { setState, updateState } = useState({})
+
+  
+  useEffect(()=>{
+
+    const fetchEvent = async () =>{
+      try {
+        const response = await fetch('http://localhost:3000/')
+        const data = await response.json();
+        console.log("data from backend: ", data)
+        setState(data);
+      }catch (err){
+        console.log(` ${err}`)
+      }
+    }
+    fetchEvent();
+  }, [])
+   //[] dependancies for when it will fire again 
+   //when the form is submitted, it should fire again with a delay 
+
+   
+
   return (
-      // {dummyEventList}
+      // <Event key={index}   />
+      <div className='cardContainer'>
+      {/* {setState.map(events=>{
+        return (
+          <div className='cardContainer'>
+            <Event eventName={events.eventName}
+            User={events.user}
+            day={events.day}
+            start={events.start}
+            end={end}
+            />
+          </div>
+        )
+      })} */}
       <Event />
+      <Event />
+      <Event />
+      <Event />
+      <Event />
+      <Event />
+      <Event />
+      <Event />
+      </div>
   );
 }
 
