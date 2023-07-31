79e5e661eef270349420831cd7ec769d99b158fc
David Lee <31666714+GomDave@users.noreply.github.com>
2023-07-30 19:09:38 -0700

    Add react router and dummy login page

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from './themes';
+
 import MainContainer from "./components/MainContainer";
+import Login from "./components/Login";
 
 const App = () => {
   return (
   
     <ThemeProvider theme={theme}>
       <CssBaseline enableColorScheme />
-        <MainContainer />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<MainContainer />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+          </Routes>
+        </BrowserRouter>
     </ThemeProvider>
   );
 }
---
+++ client/components/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Login(props) {
+  return (
+    <div>Login Placeholder</div>
+  )
+}
+
+export default Login;
