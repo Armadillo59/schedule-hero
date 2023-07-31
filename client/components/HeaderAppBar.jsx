62ac4d2eb1a7977367c82217f996c8f5d3070dd3
David Lee <31666714+GomDave@users.noreply.github.com>
2023-07-30 18:30:24 -0700

    Add nav bar component layout

---
+++ client/components/HeaderAppBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AppBar from '@mui/material/AppBar';
+
+function HeaderAppBar(props) {
+  return (
+    <AppBar position='static'>
+      SCHEDULE HERO
+    </AppBar>
+  )
+}
+
+export default HeaderAppBar;
--- client/components/MainContainer.jsx
+++ client/components/MainContainer.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import EventBoard from './EventBoard';
+import HeaderAppBar from "./HeaderAppBar";
 
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
+
 function MainContainer() {
   return (
-    <Container maxWidth="xl">
+    <Container sx={{padding: 0}}>
+      <HeaderAppBar />
       <EventBoard />
     </Container>
   );
